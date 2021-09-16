2b6f8bae27422fdca9230d845dc802fc5ed8b022
abe haile <nalfmalf@gmail.com>
2021-09-16 09:45:25 -0400

    Update transaction.ex

--- lib/transaction.ex
+++ lib/transaction.ex
@@ -7,6 +7,7 @@ defmodule River.Transaction do
     field(:id, :id)
     field(:date, :date)
     field(:tx_type, :string)
+    # I did this because I discovered a bug in ecto during this assignment. Tracked here https://github.com/elixir-ecto/ecto/issues/3748. Vindicated by Jose himself
     field(:price, BetterDecimal, scale: 2)
     field(:quantity, BetterDecimal, scale: 8)
   end
